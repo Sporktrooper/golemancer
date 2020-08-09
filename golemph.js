4d88262737bec11ff0b3b045bf352a78add3e182
Jeff Tatem <jeff.tatem@gmail.com>
2020-08-09 17:17:23 -0400

    Update golemph.js

--- golemph.js
+++ golemph.js
@@ -10,7 +10,7 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-
+var dummytext;
 
 function preload ()
 {
@@ -20,9 +20,10 @@ function preload ()
 function create ()
 {
   this.add.image(400,300,'golem');
-  this.add.text(0,0,'Hello world',{font: "14pt Arial"});
+  dummytext = this.add.text(0,0,'Hello world',{font: "14pt Arial"});
 }
 
 function update ()
 {
+  dummytext = this.add.text(0,0,'update fired');
 }
