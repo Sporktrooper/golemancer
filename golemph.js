ac5e83e4a12682f5e6cc8129aae1ecce7066de08
Jeff Tatem <jeff.tatem@gmail.com>
2020-08-09 18:05:31 -0400

    Update golemph.js

--- golemph.js
+++ golemph.js
@@ -25,11 +25,25 @@ function create ()
 {
   golemimage = this.add.sprite(400,300,'golem');
   dummytext = this.add.text(0,0,'Hello world',{font: "14pt Arial"});
+  this.start = this.getTime();
 }
 
 function update ()
 {
   golemimage.angle += 1;
   ticks++;
-  dummytext.text = 'hello world 10fps ' + ticks + ' ' + game.time.deltaTime;
+  dummytext.text = 'hello world 10fps ' + ticks;
+}
+
+function getTime ()
+{
+  var d = new Date();
+  return d.getTime();
+}
+
+function showDelta ()
+{
+  var elapsed = this.getTime()-this.start;
+  console.log("delta time "+ elapsed);
+  this.start = this.getTime();
 }
