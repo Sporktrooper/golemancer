4d86b76c8ac37ca85cb333891fe1e33b57435d69
Jeff Tatem <jeff.tatem@gmail.com>
2020-08-12 20:21:06 -0400

    Update golemph.js

--- golemph.js
+++ golemph.js
@@ -57,7 +57,6 @@ function create ()
 
   // Create the brass vial frame and its properties
   var vialText = this.add.text(350, 250, '', { font: '16px Courier', tint: '#0xb19d12'});
-  vial = this.add.image(150, 300, 'vial');
   vial.setDataEnabled();
   vial.data.set('name', 'Vial of Substance');
   vial.data.set('capacity', 50);
