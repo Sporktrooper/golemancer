1fde7b3ee5f4825036d7bc6d138dcb979516797e
Sporktrooper <jeff.tatem@gmail.com>
2020-08-30 11:00:03 -0400

    reorganized, separated seizure from riddle

---
+++ demos/formula/formula.js
@@ -0,0 +1,14 @@
+var jug5 = {
+  capacity: 5,
+  contents: 0,
+};
+var jug3 = {
+  capacity: 5,
+  contents: 0,
+};
+
+
+function fillContainer(container) {
+  container.contents = container.capacity;
+  alert(container +" should be filled.")
+}
